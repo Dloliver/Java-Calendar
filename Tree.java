d46c1e6bca4db6e8fbadec81599cee0cc613e569
dennisloliver <doliver9110@gmail.com>
2022-01-04 10:59:47 -0500

    created tree java file

---
+++ Tree.java
@@ -0,0 +1,18 @@
+// Define your tree class in this file
+
+public class Tree {
+  public static void main (String[]args){
+    //This line will introduce me and add a tree to the screen.
+    System.out.println("Hello, I', Dennis and I'm learning to code in Java");
+
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+    System.out.println("*");
+    System.out.println("*");
+    System.out.println("*");
+  }
+}
